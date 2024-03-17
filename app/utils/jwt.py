fffc294631caf31d509d4f67a1a4d13dfdae14e0
adewale-codes <adewaleabiola78@gmail.com>
2024-03-17 17:53:45 +0100

    fixed jose error

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from fastapi import Depends, HTTPException, status
-import jose as jwt
+from jose import jwt
 from datetime import datetime, timedelta
 from app.utils.security import pwd_context
 
